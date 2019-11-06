41d2176ee24cf915c9bdab4173a83607762dbb5f
Esin Saribudak <esin.saribudak@gmail.com>
2019-11-06 14:11:22 -0600

    plant list, show, css

---
+++ plant_mom/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.plants_list, name='plants_list'),
+    path('plants/<int:pk>', views.plant_detail, name='plant_detail'),
+]
--- plant_mom/views.py
+++ plant_mom/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Plant
 # Create your views here.
+
+
+def plants_list(request):
+    plants = Plant.objects.all()
+    return render(request, 'plant_mom/plants_list.html', {'plants': plants})
+
+
+def plant_detail(request, pk):
+    plant = Plant.objects.get(id=pk)
+    return render(request, 'plant_mom/plant_detail.html', {'plant': plant})
