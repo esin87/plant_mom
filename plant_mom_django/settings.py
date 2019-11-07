e32e7feca1cd0d09df644f23abedfb122211b6f0
Esin Saribudak <esin.saribudak@gmail.com>
2019-11-06 21:40:57 -0600

    styled login page

--- plant_mom_django/settings.py
+++ plant_mom_django/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'plant_mom',
     'social_django',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
